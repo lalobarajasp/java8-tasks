063d55fc6cd5558626f6caf265956eff5fbd7a9c
lalobarajasp <ing.barajas.perez@gmail.com>
2023-04-10 17:40:57 -0600

    First commit

---
+++ src/main/java/com/expertsoft/model/Product.java
@@ -0,0 +1,61 @@
+package com.expertsoft.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    public enum Color {
+        WHITE,
+        BLACK,
+        RED,
+        BLUE,
+        YELLOW
+    }
+
+//    The empty constructor can be used when we want to make sure that any instance created
+//    is always valid and the variables are initialized.
+
+    public Product() {}
+
+    private Long id;
+    private String name;
+    private Color color;
+    private BigDecimal price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product #" + id + " (" + name + ")";
+    }
+}
